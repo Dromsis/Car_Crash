061397662e2bd972ed01b79c019f2b38ed570eae
DROMSIS <96498216+Dromsis@users.noreply.github.com>
2022-01-24 16:35:16 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,26 @@
+
+local initialHealth = -1
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(10)
+        --print("11111")
+        local vehicle = GetVehiclePedIsIn(PlayerPedId(),false)
+        local engineHealth = GetVehicleEngineHealth(vehicle)
+        if initialHealth == -1 then
+            initialHealth = engineHealth
+        else
+            if engineHealth <= 500 then 
+                SetVehicleFuelLevel(vehicle,0.0)
+            elseif engineHealth <= initialHealth-30 then
+                local initialFuel = GetVehicleFuelLevel(vehicle)
+                Wait(10)
+                SetVehicleFuelLevel(vehicle,0.0)
+                Wait(5000)
+                SetVehicleFuelLevel(vehicle,initialHealth)
+            end
+            initialHealth = engineHealth
+        end
+
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Dromsis'
+description 'A lua adaptation of '
+version '1.0'
+
+client_scripts {
+	'client.lua'
+}
